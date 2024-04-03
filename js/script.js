753b927c3bddb98bec81d105c1536f329595751e
George Khalil <159185251+George-Khalil-29@users.noreply.github.com>
2024-04-03 00:46:38 +0000

    save

--- js/script.js
+++ js/script.js
@@ -1,5 +1,23 @@
-// Copyright (c) 2020 Mr. Coxall All rights reserved
+// Copyright (c) 2024 George, Khalil All rights reserved
 //
-// Created by: Mr. Coxall
-// Created on: Sep 2020
+// Created by: George , Khalil
+// Created on: Apr 2024
 // This file contains the JS functions for index.html
+
+"use strict"
+
+/**
+ * This function calculates area of a triangle.
+ */
+function AreaOfTrapizoid() {
+  // input
+  const ABase = parseInt(document.getElementById('A-Base').value)
+  const BBase = parseInt(document.getElementById('B-Base').value)
+  const Height = parseInt(document.getElementById('Height').value)
+  // process
+  const areaOfTrapizoid = [(ABase + BBase) / 2] * Height
+
+  // output
+  document.getElementById('area').innerHTML = 'Area is: ' + ABase + BBase / 2 * Height + ' mm²'
+
+}
